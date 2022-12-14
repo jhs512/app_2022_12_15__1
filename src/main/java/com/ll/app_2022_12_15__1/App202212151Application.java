6d24215f90b5bdce855554f562097c555cebd987
jhs512 <jangka512@gmail.com>
2022-12-15 04:33:04 +0900

    세팅

---
+++ src/main/java/com/ll/app_2022_12_15__1/App202212151Application.java
@@ -0,0 +1,13 @@
+package com.ll.app_2022_12_15__1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App202212151Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App202212151Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/app_2022_12_15__1/App202212151ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.app_2022_12_15__1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App202212151ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
